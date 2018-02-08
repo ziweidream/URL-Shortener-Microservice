b4b7d61fea968461d5fe2aa05be92abbcdb4f311
Glitch (supreme-save) <none>
2018-02-08 16:50:01 +0000

    💀⛩ Checkpoint server.js:621255/117

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 // server.js
 // where your node app starts
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://vivi:123@ds217898.mlab.com:17898/url-shortener";
+
 // init project
 var express = require('express');
 var app = express();
 
-function shortenUrl(){ 
+function shortenUrl(originalUrl){ 
   var shortened = 'https://supreme-save.glitch.me/' + random() + random() + random() + random();
   function random() {
     var num = Math.floor(Math.random() * 10);
     return num.toString();
   }
+  var url = "mongodb://vivi:123@ds217898.mlab.com:17898/url-shortener";
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("url-shortener");
+  var obj = { original: originalUrl, short: shortened };
+  dbo.collection("urls").insertOne(obj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
   return shortened;
 }
 
@@ -34,7 +45,7 @@ app.get("/new/*", function (request, response) {
   var myobj = {};
   if (request.params[0].substring(0, 4) === "http" && isUrlValid(request.params[0])) {
     myobj.originalUrl = request.params[0];
-    myobj.shortUrl = shortenUrl();
+    myobj.shortUrl = shortenUrl(request.params[0]);
   } else {
     myobj.error = "Wrong url format, please make sure you have a valid protocol and real site."
   }
