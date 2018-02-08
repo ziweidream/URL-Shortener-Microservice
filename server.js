a3d5d9cee131fe5d777022931c7be2385086387d
Glitch (supreme-save) <none>
2018-02-08 14:47:08 +0000

    🎩⛄️ Checkpoint server.js:621255/319

--- server.js
+++ server.js
@@ -5,17 +5,13 @@
 var express = require('express');
 var app = express();
 
-var alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+var alphabet = "0123456789";
 var base = alphabet.length; // base is the length of the alphabet (58 in this case)
 // utility function to convert base 10 integer to base 58 string
-function encode(num){
-  var encoded = '';
-  while (num){
-    var remainder = num % base;
-    num = Math.floor(num / base);
-    encoded = alphabet[remainder].toString() + encoded;
-  }
-  return encoded;
+function shortenUrl(str){
+  var shortened = '';
+  
+  return shortened;
 }
 
 // http://expressjs.com/en/starter/static-files.html
@@ -27,8 +23,11 @@ app.get("/", function (request, response) {
 });
 
 app.get("/new/:url", function (request, response) {
-  var shortUrl = 
-  response.send(request.params.url);
+ 
+  var myobj = {};
+  myobj.originalUrl = request.params.url;
+  myobj.shortUrl = encode(request.params.url);
+  response.send(myobj);
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
